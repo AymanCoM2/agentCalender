6f45a687ebd1091d86756bda8a6a8792d39e5bcf
Ayman <ayman.salah@2coom.com>
2023-12-30 12:26:01 +0200

    Added the Loop For the Dates

--- public/js/cellCheck.js
+++ public/js/cellCheck.js
@@ -8,46 +8,57 @@ let dayCells = document.querySelectorAll(".inner_cell");
 //   _: "_",
 // };
 
+
 dayCells.forEach((eachCell) => {
-  let currentSymbol = eachCell.dataset.currentSymbo;
-  eachCell.addEventListener("click", function () {
-    // Toggle through symbols
-    switch (currentSymbol) {
-      case "_":
-        currentSymbol = "X"; // Clear the cell
-        eachCell.style.backgroundColor = "white";
-        eachCell.dataset.currentSymbol = currentSymbol;
-        break;
-      case "X":
-        currentSymbol = "O"; // Clear the cell
-        eachCell.style.backgroundColor = "Orange";
-        eachCell.dataset.currentSymbol = currentSymbol;
-        break;
-      case "O":
-        currentSymbol = "F"; // Change to "F" on the next click
-        eachCell.style.backgroundColor = "red";
-        eachCell.dataset.currentSymbol = currentSymbol;
-        break;
-      case "F":
-        currentSymbol = "P"; // Change to "P" on the next click
-        eachCell.style.backgroundColor = "green";
-        eachCell.dataset.currentSymbol = currentSymbol;
-        break;
-      case "P":
-        currentSymbol = "N"; // Change to "N" on the next click
-        eachCell.style.backgroundColor = "blue";
-        eachCell.dataset.currentSymbol = currentSymbol;
-        break;
-      case "N":
-        currentSymbol = "_"; // Change to "" (clear) on the next click
-        eachCell.style.backgroundColor = "white";
-        eachCell.dataset.currentSymbol = currentSymbol;
-        break;
-      default:
-        break;
+    let currentSymbol = eachCell.dataset.currentSymbo;
+    let taskDate = eachCell.dataset.taskDate;
+    let taskMonth = eachCell.dataset.taskMonth;
+    let cardCode = eachCell.dataset.cardCode;
+    let repId = eachCell.dataset.repId;
+
+    function saveCellData(_symbolState , _taskDate , _taskMonth , _cardCode , _repId)
+    {
+      
     }
-    if (currentSymbol) {
-      eachCell.innerText = currentSymbol;
-    }
-  });
+
+    eachCell.addEventListener("click", function () {
+        // Toggle through symbols
+        switch (currentSymbol) {
+            case "_":
+                currentSymbol = "X"; // Clear the cell
+                eachCell.style.backgroundColor = "white";
+                eachCell.dataset.currentSymbol = currentSymbol;
+                break;
+            case "X":
+                currentSymbol = "O"; // Clear the cell
+                eachCell.style.backgroundColor = "Orange";
+                eachCell.dataset.currentSymbol = currentSymbol;
+                break;
+            case "O":
+                currentSymbol = "F"; // Change to "F" on the next click
+                eachCell.style.backgroundColor = "red";
+                eachCell.dataset.currentSymbol = currentSymbol;
+                break;
+            case "F":
+                currentSymbol = "P"; // Change to "P" on the next click
+                eachCell.style.backgroundColor = "green";
+                eachCell.dataset.currentSymbol = currentSymbol;
+                break;
+            case "P":
+                currentSymbol = "N"; // Change to "N" on the next click
+                eachCell.style.backgroundColor = "blue";
+                eachCell.dataset.currentSymbol = currentSymbol;
+                break;
+            case "N":
+                currentSymbol = "_"; // Change to "" (clear) on the next click
+                eachCell.style.backgroundColor = "white";
+                eachCell.dataset.currentSymbol = currentSymbol;
+                break;
+            default:
+                break;
+        }
+        if (currentSymbol) {
+            eachCell.innerText = currentSymbol;
+        }
+    });
 });
--- routes/web.php
+++ routes/web.php
@@ -2,52 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-function getMonthDatesWithName($monthNumber)
-{
-    if ($monthNumber < 1 || $monthNumber > 12) {
-        return false;
-    }
-    $currentYear = date('Y');
-    $numDays = cal_days_in_month(CAL_GREGORIAN, $monthNumber, $currentYear);
-    $monthDatesWithNames = [];
-    for ($day = 1; $day <= $numDays; $day++) {
-        $fullDate = sprintf('%04d-%02d-%02d', $currentYear, $monthNumber, $day);
-        $date = new DateTime($fullDate);
-        $dayName = $date->format('D');
-        $monthDatesWithNames[$fullDate] = $dayName;
-    }
-    return $monthDatesWithNames;
-}
-
-
-function cutMonthArrayIntoWeeks($monthArray)
-{
-    // Define the weeks
-    $weeks = [
-        'week_1' => [],
-        'week_2' => [],
-        'week_3' => [],
-        'week_4' => [],
-        'week_5' => [],
-    ];
-
-    // Iterate through the days and distribute them into weeks
-    foreach ($monthArray as $date => $day) {
-        // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
-        $dayOfWeek = date('w', strtotime($date));
-
-        // Calculate the week index
-        $weekIndex = intval(($dayOfWeek + date('j', strtotime($date)) - 1) / 7) + 1;
-
-        // If the week index is greater than 5, put it in week 5
-        $weekIndex = min($weekIndex, 5);
-
-        // Add the date to the corresponding week
-        $weeks["week_$weekIndex"][$date] = $day;
-    }
-
-    return $weeks;
-}
 function establishConnectionDB($inputQuery)
 {
     $serverName = "jou.is-by.us";
@@ -65,9 +19,51 @@ function establishConnectionDB($inputQuery)
     return $stmt;
 }
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+function getMonthDatesWithNames($monthNumber)
+{
+    // This Function Gets The Month Number [ 12 For December ]
+    // And Then Return Associative array [ Key is the Date & Value is the Name "ie : sat"]
+    if ($monthNumber < 1 || $monthNumber > 12) {
+        return false;
+    }
+    $currentYear = date('Y');
+    $numDays = cal_days_in_month(CAL_GREGORIAN, $monthNumber, $currentYear);
+    $monthDatesWithNames = [];
+    for ($day = 1; $day <= $numDays; $day++) {
+        $fullDate = sprintf('%04d-%02d-%02d', $currentYear, $monthNumber, $day);
+        $date = new DateTime($fullDate);
+        $dayName = $date->format('D');
+        $monthDatesWithNames[$fullDate] = $dayName;
+    }
+    return $monthDatesWithNames;
+}
+
+function cutMonthArrayIntoWeeks($monthArray)
+{
+    // This function takes the month array and Split it Into Weeks 
+    $weeks = [
+        'week_1' => [],
+        'week_2' => [],
+        'week_3' => [],
+        'week_4' => [],
+        'week_5' => [],
+    ];
+    // Iterate through the days and distribute them into weeks
+    foreach ($monthArray as $date => $day) {
+        // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+        $dayOfWeek = date('w', strtotime($date));
+        // Get the week index based on the day of the month
+        $weekIndex = ceil(date('j', strtotime($date)) / 7);
+        // If the week index is greater than 5, put it in week 5
+        $weekIndex = min($weekIndex, 5);
+        // Add the date to the corresponding week
+        $weeks["week_$weekIndex"][$date] = $day;
+    }
+
+    return $weeks;
+}
+
 
 Route::get('/fill-calender', function () {
     $sampleSqlQuery  = "
@@ -82,12 +78,17 @@ Route::get('/fill-calender', function () {
         $clientsDataArrray[] = $row;
     }
     // Making Connection For Customers
-    $daysArray = getMonthDatesWithName(12);
-    // dd($daysArray);
+    $daysArray = getMonthDatesWithNames(12); // ! 1 
     // dd($clientsDataArrray);
-    $weeko = cutMonthArrayIntoWeeks($daysArray);
-    dd($weeko);
-    // Checking How Many Weeks We Have 
+    $weeksArray = cutMonthArrayIntoWeeks($daysArray);
+    // dd($weeksArray);
+    // Checking How Many Weeks We Have
     // Checking How Many Days 
-    return view('fill-calender');
+    return view('fill-calender', compact(['weeksArray', 'clientsDataArrray']));
 })->name('fill-calender-get');
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
